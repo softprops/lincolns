2a79e9ef06cbbb4adbb0985484babb976c42e2cf
softprops <softprops@users.noreply.github.com>
2019-09-23 12:31:11 +0000

    Automated deployment: Mon Sep 23 12:31:11 UTC 2019 d174958a804dae293c6e679befe908583e3958bb

--- aliases.js
+++ aliases.js
@@ -1,4 +1,6 @@
 var ALIASES = {};
+ALIASES["cfg_if"] = {};
 ALIASES["lincolns"] = {};
 ALIASES["linked_hash_map"] = {};
+ALIASES["log"] = {};
 ALIASES["yaml_rust"] = {};
--- source-files.js
+++ source-files.js
@@ -1,5 +1,7 @@
 var N = null;var sourcesIndex = {};
+sourcesIndex["cfg_if"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["lincolns"] = {"name":"","files":["error.rs","lib.rs","path.rs","position.rs"]};
 sourcesIndex["linked_hash_map"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["log"] = {"name":"","files":["lib.rs","macros.rs"]};
 sourcesIndex["yaml_rust"] = {"name":"","files":["emitter.rs","lib.rs","parser.rs","scanner.rs","yaml.rs"]};
 createSourceSidebar();
